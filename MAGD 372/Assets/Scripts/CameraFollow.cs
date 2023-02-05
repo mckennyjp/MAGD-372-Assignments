4d3f0310d6345f5d84064e76be1fb9ec7fbfef21
mckennyjp <jumbomax792@gmail.com>
2023-02-05 12:49:58 -0600

    Finished Scene Transition and added SplitScreen

---
+++ MAGD 372/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    [SerializeField] private Vector3 offset;
+
+    [SerializeField] [Range(0.01f, 1f)]
+
+    private float smoothSpeed = 0.125f;
+
+    private Vector3 velocity = Vector3.zero;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+      
+    }
+    private void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothSpeed);
+    }
+}
